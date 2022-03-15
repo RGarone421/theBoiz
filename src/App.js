2fe74a69cc03ead152ccfab4e74d929308294d71
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-03-15 13:58:36 -0400

    created base screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+const App = () => {
+  const [item, setItem]=useState('')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <>
+    <h1>Re-Play</h1>
+    <button>Home</button>
+    <button>Login</button>
+    <button>Listings</button>
+    <button>Create a Listing</button>
+    </>
+  )
 }
 
 export default App;
