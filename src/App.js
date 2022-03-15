9e218b7f300bd5fdac9e3c4240fddb94b4a636b2
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-03-15 14:03:27 -0400

    button functions done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import './App.css';
 import {useEffect, useState} from 'react'
 const App = () => {
   const [item, setItem]=useState('')
+  const [homeCheck, setHomeCheck]=useState(true)
+  const [loginCheck, setLoginCheck]=useState(false)
+  const [listingCheck, setListingCheck]=useState(false)
+  const [createCheck, setCreateCheck]=useState(false)
 
+  const goToHome = () => {
+    setLoginCheck(false)
+    setListingCheck(false)
+    setCreateCheck(false)
+    setHomeCheck(true)
+  }
+  const goLogin = () => {
+    setLoginCheck(true)
+    setListingCheck(false)
+    setCreateCheck(false)
+    setHomeCheck(false)
+  }
+  const goToListings = () => {
+    setListingCheck(true)
+    setLoginCheck(false)
+    setCreateCheck(false)
+    setHomeCheck(false)
+  }
+  const goToCreate = () => {
+    setCreateCheck(true)
+    setLoginCheck(false)
+    setHomeCheck(false)
+    setListingCheck(false)
+  }
   return(
     <>
     <h1>Re-Play</h1>
