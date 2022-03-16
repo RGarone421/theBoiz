357cf774c8028845b9f495169d05b3cab08224b6
Robert Garone <robgarone@me.com>
2022-03-16 11:22:44 -0400

    style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home'
+
+
+
 const App = () => {
   const [item, setItem]=useState('')
   const [homeCheck, setHomeCheck]=useState(true)
@@ -42,21 +45,7 @@ const App = () => {
     setListingCheck(false)
   }
   return(
-    <>
-    <h1>Re-Play</h1>
-    <button onClick={goToHome}>Home</button>
-    <button onClick={goLogin}>Login</button>
-    <button onClick={goToListings}>Listings</button>
-    <button onClick={goToCreate}>Create a Listing</button>
-    {homeCheck ? (
-      <>
-      <div class='container'>
-        <h1>Re-Play</h1>
-        <p>We know how much you like the nostolgia of old times, that's why, we're happy to present <b>Re-Play</b> a new store front that allows for listings of all things vintage at a good price. It's up to us as a community to stop scalpers, and those taking the joy out of the hobby. Let's help eachother out so we can all <i>Re-Play</i> our old favorites.</p>
-        </div>
-        </> ): null
-    }
-    </>
+    <Home />
   )
 }
 
