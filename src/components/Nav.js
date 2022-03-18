af81665c2186f80eb519f550ae4364ace9a60cc2
Robert Garone <robgarone@me.com>
2022-03-18 13:58:21 -0400

    fancy

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -21,7 +21,7 @@ const Nav = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         </ul>
-        <button onClick={changeAddCheck}>Add a New Listing</button>
+        {/* <button className='btn btn-info btn-sm' onClick={changeAddCheck}>Create Listing</button> */}
         <form className="d-flex">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success" type="submit">Search</button>
